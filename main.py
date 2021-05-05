7fa7594108515faa428c9ec43c7b3eec9410a0c4
Rishav Das <rdofficial192@gmail.com>
2021-05-05 22:02:34 +0530

    Added the sign up option on the login screen as well as added the signup functionality on this script. Added the feature of detecting the blank usernames or password entries by the user in case of both login or signup. Updated some parts of the code + changed the comment texts in order to make the comments to describe more of the code.

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ Last modified by : Rishav Das (https://github.com/luckyverma-sudo)
 Last modified on : May 5, 2021
 
 Changes made in last modification :
-1. Changed the entire structure of the project, removed files, replaced the main script with the main.py file.
-2. Created main.py file + added the complete set of error free code into it.
-3. Added proper data storing and fetching functionality to the script. Changed the old ways of data storing as it was full of errors and mistakes, new way of the data storing is using JSON files. We will store the user credentials in the data.json file, and for each login verification we will check for the existing (already stored) user credentials in that file. Added the function 'authenticate(username, password)'' which is automatically called with passing correct arguments, when the user presses the login button on tkinter window of this tool. Also the function then checks wheter the user entered credentials are correct or not with the existing user credentials in the data.json file. If the user enters a correct username and password, then we continue to welcome the newly logged in user, else if the user enters wrong username and password combination, then we raise an error message back to the user. The error messages as well as the other info messages are also created using the messagebox class of the tkinter module. The script currently uses two modules : tkinter and json. Tkinter module is used for creating the graphical objects and the entire GUI window, and on the other hand we use the json module to work with the json data in our application. The sign up feature will be added soon.
+1. Added the sign up option on the login screen as well as added the signup functionality on this script.
+2. Added the feature of detecting the blank usernames or password entries by the user in case of both login or signup.
+3. Updated some parts of the code + changed the comment texts in order to make the comments to describe more of the code.
 
 Authors contributed to this script (Add your name below if you have contributed) :
 1. Lucky Verma (github:https://github.com/luckyverma-sudo/, email:@gmail.com)
@@ -28,6 +28,82 @@ except Exception as e:
 	# If there are any errors encountered during the importing of the modules, then we display the error on the console screen
 
 	input(f'\n[ Error : {e} ]\nPress enter key to continue...')
+
+def createUser(username, password):
+	""" This function creates a new user item using the user provided credentials and then saves the updated data in the data.json file. The function takes in two arguments : username, password. The username and password are the inputs that the user enters on the signup screen input boxes. """
+
+	if (len(username) >= 5) and (len(password) >= 8) and (' ' not in username):
+		# If the user entered username and password inputs are valid and then we continue
+
+		# Loading the user credentials from data.json file
+		data = loads(open('data.json', 'r').read())
+
+		# Checking the username and password pre-existence in the data
+		for item in data:
+			if item["username"] == username:
+				# If the user entered username already exists for a different user, then we display the error message to the user
+
+				mb.showerror('Username exists', 'Another user with the same username exists. Please choose any other username.')
+				return 0
+		
+		# If we are out of the loop, then we assume that no other user exist with the same username
+		# Creating the new user item
+		data.append({"username" : username, "password" : password})
+
+		# Saving the data back to the data.json file
+		open('data.json', 'w+').write(dumps(data))
+		
+		# Displaying the success message to the user and then switching to the login screen
+		mb.showinfo('Sign up success', 'New user has been created using the specified username and password. Go to login page to test it out now.')
+		signupWin.destroy()
+		main()
+		return 1
+	else:
+		# If the user entered username and password inputs are not valid, then we display the error message back to the user
+
+		mb.showerror('Invalid inputs', 'Please enter proper input for username and password. The username should be atleast 5 characters long as well as it should be a alphanumeric term and should not contain any kind of symbols or special characters. The password should also be either equal or more than 8 characters and the password can include special characters.')
+		return 0
+
+def createSignupWin():
+	""" This function creates the sign up screen on the main tkinter window of this script. The function takes no argument. """
+
+	# Allowing some variables defined in this function to be globally accessed
+	global signupWin
+
+	# Creating a new window for the sign up box + destroying the older one
+	win.destroy()
+	signupWin = Tk()
+	signupWin.title('Sign Up - Login System')
+	signupWin.geometry('500x250')
+	signupWin.resizable(0, 0)
+	signupWin.config(background = 'black')
+
+	# ----
+	# Creating the widgets for the sign up window
+	# ----
+	# Creating the centered heading of the window
+	Label(signupWin, text = 'Sign Up', foreground = 'green', background = 'black', font = ('', 20, 'bold', 'italic'),).pack(padx = 10, pady = 10)
+
+	# Defining the username entry part
+	frame1 = Frame(signupWin, background = 'black')
+	frame1.pack(padx = 5, pady = (10, 5))
+	Label(frame1, text = 'Username', foreground = 'green', background = 'black', font = ('', 12, '')).pack(side = LEFT, padx = 5)
+	username = StringVar(signupWin)  # Defining a string var for storing the user entered username
+	Entry(frame1, textvariable = username, font = ('', 12, '')).pack(side = RIGHT, padx = 5)
+
+	# Defining the password entry part
+	frame2 = Frame(signupWin, background = 'black')
+	frame2.pack(padx = 5, pady = (5, 10))
+	Label(frame2, text = 'Password', foreground = 'green', background = 'black', font = ('', 12, '')).pack(side = LEFT, padx = 5)
+	password = StringVar(signupWin)  # Defining a string var for storing the user entered password
+	Entry(frame2, textvariable = password, font = ('', 12, ''), show = '*').pack(side = RIGHT, padx = 5)
+
+	# Defining the sign up button
+	signupBtn = Button(signupWin, text = 'Sign up', foreground = 'black', background = 'green', activeforeground = 'green', activebackground = 'black', font = ('', 12, ''), relief = GROOVE, command = lambda : createUser(username.get(), password.get()))
+	signupBtn.pack(pady = 5, padx = 5)
+	# ----
+
+	mainloop()
 
 def authenticate(username, password):
 	""" This function authenticates the user on the basis of the credentials provided using the arguments. This function takes two arguments (parameters) : username, password. When the user enters the username and passwords, this function should be called as well as the user entered data should be passed to it. The function can also be added to the onclick event listener of the login button. """
@@ -51,6 +127,7 @@ def authenticate(username, password):
 				frame1.destroy()
 				frame2.destroy()
 				loginBtn.destroy()
+				signupBtn.destroy()
 
 				# Writting the new label
 				Label(win, text = f'Logged in as : {username}', foreground = 'green', background = 'black', font = ('', 12, '')).pack(padx = 5, pady = 5)
@@ -71,12 +148,12 @@ def authenticate(username, password):
 def main():
 	# DRIVER CODE
 
-	# Making some variables declared in this function global
-	global win, frame1, frame2, loginBtn
+	# Allowing some variables defined in this function to be globally accessed
+	global win, frame1, frame2, loginBtn, signupBtn
 
 	# Defining the tkinter window
 	win = Tk()
-	win.title('Login box')
+	win.title('Log In - Login System')
 	win.geometry('500x250')
 	win.resizable(0, 0)
 	win.config(background = 'black')
@@ -110,7 +187,12 @@ def main():
 	# Defining the login button
 	loginBtn = Button(win, text = 'Log In', foreground = 'black', background = 'green', activeforeground = 'green', activebackground = 'black', font = ('', 12, ''), relief = GROOVE, command = lambda : authenticate(username.get(), password.get()))
 	loginBtn.pack(pady = 5, padx = 5)
+
+	# Defining the signup button for switching to it whenever the user wants to create a new user account
+	signupBtn = Button(win, text = 'Sign up', foreground = 'black', background = 'green', activeforeground = 'green', activebackground = 'black', font = ('', 12, ''), relief = GROOVE, command = createSignupWin)
+	signupBtn.pack(pady = 5, padx = 5)
 	# ----
+
 	mainloop()
 
 if __name__ == '__main__':
